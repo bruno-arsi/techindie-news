37cf02fdb27f40461861118b533ec74b2f1c49e3
Arsi <124640248+bruno-arsi@users.noreply.github.com>
2025-08-30 17:01:09 +0000

    adjusting asset ref

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import closedBox from "../assets/closed_box.png";
+import openBox from "../assets/open_box.png";
 
-var {isOpen, setIsOpen} = useState<Boolean>(false);
+var { isOpen, setIsOpen } = useState < Boolean > (false);
 
-function Home(){
-    if(isOpen){
-        return <img src="assets/open_box.png" alt="Presente"/>
+function Home() {
+    if (isOpen) {
+        return <img src={openBox} alt="Presente" />
     }
 
-    return <img onClick={() => setIsOpen(true)} src="assets/closed_box.png" alt="Presente"/>
+    return <img onClick={() => setIsOpen(true)} src={closedBox} alt="Presente" />
 }
 
 export default Home;
