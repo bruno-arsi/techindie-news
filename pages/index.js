db7b0fc82ffa8db8d2485e0de2e99c58a724bb12
Arsi <124640248+bruno-arsi@users.noreply.github.com>
2025-08-30 16:54:52 +0000

    closed and open box with setState

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import { useState } from "react";
+
+var {isOpen, setIsOpen} = useState<Boolean>(false);
+
 function Home(){
-    return <h1>GM World!</h1>
+    if(isOpen){
+        return <img src="assets/open_box.png" alt="Presente"/>
+    }
+
+    return <img onClick={() => setIsOpen(true)} src="assets/closed_box.png" alt="Presente"/>
 }
 
 export default Home;
