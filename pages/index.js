6ccb1be54faa45d89e745da5c54d8164a3b5ba4c
Arsi <124640248+bruno-arsi@users.noreply.github.com>
2025-08-30 17:20:59 +0000

    presente pra quem quer se tornar um dev melhor

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 import { useState } from "react";
-import closedBox from "../assets/closed_box.png";
-import openBox from "../assets/open_box.png";
-
-var { isOpen, setIsOpen } = useState < Boolean > (false);
 
 function Home() {
-    if (isOpen) {
-        return <img src={openBox} alt="Presente" />
-    }
+    const closedBox = "/assets/closed_box.png";
+    const openBox = "/assets/open_box.png";
+    const [isOpen, setIsOpen] = useState(false);
 
-    return <img onClick={() => setIsOpen(true)} src={closedBox} alt="Presente" />
+    return (
+        <div>
+            {isOpen ? (
+                <div>
+                    <h1>Você foi programaticado!</h1> <br />
+                    <h2>Pra se tornar um desenvolvedor melhor, dá uma risadinha</h2>
+                    <img width={640} height={640} src={openBox} alt="Presente aberto" />
+                </div>
+            ) : (
+                <div>
+                    <h1>Tenho um presente pra você!</h1> <br />
+                    <img width={640} height={640} onClick={() => setIsOpen(true)} src={closedBox} alt="Presente fechado" />
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Home;
